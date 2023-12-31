baf9aac5ce28bb7d693f315e4ee9b4bc2ec75be2
t0stiman <18124323+t0stiman@users.noreply.github.com>
2023-12-31 17:57:56 +0100

    get rid of a NullReferenceException

---
+++ Mapify/Patches/CashRegisterWithModules_Patch.cs
@@ -0,0 +1,16 @@
+﻿using DV.CashRegister;
+using HarmonyLib;
+
+namespace Mapify.Patches
+{
+    [HarmonyPatch(typeof(CashRegisterWithModules), nameof(CashRegisterWithModules.Cancel))]
+    public static class CashRegisterWithModules_Cancel_Patch
+    {
+        // Gets rid of a NullReferenceException
+        private static bool Prefix(CashRegisterWithModules __instance)
+        {
+            Mapify.LogWarning($"skip {nameof(CashRegisterWithModules.Cancel)}");
+            return __instance.registerModules == null;
+        }
+    }
+}
