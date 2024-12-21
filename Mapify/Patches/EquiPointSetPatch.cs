9a0c7d306b386c94a2f6c15e804bf67abe430751
t0stiman <18124323+t0stiman@users.noreply.github.com>
2024-12-21 15:16:39 +0100

    make turntables roll

--- Mapify/Patches/EquiPointSetPatch.cs
+++ Mapify/Patches/EquiPointSetPatch.cs
@@ -1,6 +1,7 @@
 using System;
 using DV.PointSet;
 using HarmonyLib;
+using UnityEngine;
 
 namespace Mapify.Patches
 {
@@ -17,4 +18,25 @@ namespace Mapify.Patches
             return false;
         }
     }
+
+    [HarmonyPatch(typeof(EquiPointSet), nameof(EquiPointSet.RotateAroundPoint))]
+    public static class EquiPointSet_RotateAroundPoint_Patch
+    {
+        private static bool Prefix(ref EquiPointSet __instance, Vector3d rotateAnchorWorldPosition, Quaternion rotationToApply)
+        {
+            for (int index = 0; index < __instance.points.Length; ++index)
+            {
+                Vector3d vector3d = __instance.points[index].position - rotateAnchorWorldPosition;
+                Vector3 v3 = rotationToApply * (Vector3) vector3d;
+                __instance.points[index].position = rotateAnchorWorldPosition + new Vector3d(v3);
+                __instance.points[index].forward = rotationToApply * __instance.points[index].forward;
+
+                __instance.points[index].up = rotationToApply * __instance.points[index].up;
+            }
+
+            return false;
+        }
+    }
+
+
 }
--- Mapify/Patches/TurntableRailTrackPatch.cs
+++ Mapify/Patches/TurntableRailTrackPatch.cs
@@ -1,11 +1,57 @@
 using HarmonyLib;
 using Mapify.Utils;
+using UnityEngine;
+using Quaternion = UnityEngine.Quaternion;
+using Vector3 = UnityEngine.Vector3;
 
 namespace Mapify.Patches
 {
     [HarmonyPatch(typeof(TurntableRailTrack), nameof(TurntableRailTrack.RotateToTargetRotation))]
     public class TurntableRailTrack_RotateToTargetRotation_Patch
     {
+        /// roll instead of yaw
+        public static bool Prefix(ref TurntableRailTrack __instance, bool forceConnectionRefresh)
+        {
+            var currentYrotation = __instance.currentYRotation;
+            __instance.currentYRotation = __instance.targetYRotation;
+
+            var num = __instance.currentYRotation - currentYrotation;
+            if (Mathf.Abs(num) == 0.0)
+            {
+                if (!forceConnectionRefresh)
+                    return false;
+            }
+            else
+            {
+                var turnTablePosition = __instance.transform.parent.position;
+                var rotationToApplyEulers = num * __instance.visuals.forward;
+                var rotationToApply = Quaternion.Euler(rotationToApplyEulers);
+
+                for (var index = 0; index < __instance.Track.curve.pointCount; ++index)
+                {
+                    var bezierPoint = __instance.Track.curve[index];
+                    bezierPoint.position = __instance.RotateAroundPoint(bezierPoint.position, turnTablePosition, rotationToApply);
+
+                    bezierPoint.handle1 = __instance.RotateAroundPoint(bezierPoint.handle1, Vector3.zero, rotationToApply);
+                    if (bezierPoint.handleStyle != BezierPoint.HandleStyle.Connected)
+                        bezierPoint.handle2 = __instance.RotateAroundPoint(bezierPoint.handle2, Vector3.zero, rotationToApply);
+                }
+
+                __instance.Track.GetPointSet().RotateAroundPoint(new Vector3d(turnTablePosition - WorldMover.currentMove), rotationToApply);
+
+                foreach (var onTrackBogey in __instance.Track.onTrackBogies)
+                {
+                    onTrackBogey.Car.ForceOptimizationState(false);
+                    onTrackBogey.RefreshBogiePoints();
+                }
+                __instance.visuals.Rotate(rotationToApplyEulers, Space.World);
+            }
+
+            __instance.UpdateTrackConnection();
+
+            return false;
+        }
+
         public static void Postfix(TurntableRailTrack __instance)
         {
             RailwayMeshUpdater.UpdateTrack(__instance.Track);
