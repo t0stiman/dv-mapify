0e138ecc34db4d62eee3b804bc57f1cb2ac68984
t0stiman <18124323+t0stiman@users.noreply.github.com>
2025-08-14 14:13:30 +0200

    fix missing scanner in shops

---
+++ Mapify/Components/ForceActive.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Mapify.Components
+{
+    /// <summary>
+    /// Force the target Gameobject to stay active
+    /// </summary>
+    public class ForceActive: MonoBehaviour
+    {
+        public GameObject target;
+
+        private void Update()
+        {
+            if(target.activeSelf) return;
+            target.SetActive(true);
+        }
+    }
+}
---
+++ Mapify/Patches/ItemLocationForcerPatch.cs
@@ -0,0 +1,39 @@
+﻿using DV;
+using DV.Shops;
+using HarmonyLib;
+using Mapify.Components;
+using Mapify.Map;
+using VLB;
+
+namespace Mapify.Patches
+{
+    /// <summary>
+    /// Something keeps setting the shop scanner inactive. This patch prevents that.
+    /// </summary>
+    [HarmonyPatch(typeof(ItemLocationForcer), nameof(ItemLocationForcer.Awake))]
+    public class ItemLocationForcer_Awake_Patch
+    {
+        private static void Postfix(ItemLocationForcer __instance)
+        {
+            if(__instance.itemGO == null || Maps.IsDefaultMap) return;
+            if(!__instance.itemGO.TryGetComponent<ShopScanner>(out var shopScanner)) return;
+
+            Mapify.LogDebug($"Forcing scanner at '{shopScanner.gameObject.GetPath()}' active");
+            var forceActive = __instance.gameObject.GetOrAddComponent<ForceActive>();
+            forceActive.enabled = true;
+            forceActive.target = shopScanner.gameObject;
+        }
+    }
+
+    [HarmonyPatch(typeof(ItemLocationForcer), nameof(ItemLocationForcer.OnDisable))]
+    public class ItemLocationForcer_OnDisable_Patch
+    {
+        private static void Postfix(ItemLocationForcer __instance)
+        {
+            if(__instance.itemGO == null || Maps.IsDefaultMap) return;
+            if(!__instance.itemGO.TryGetComponent<ForceActive>(out var forceActive)) return;
+            forceActive.enabled = false;
+        }
+    }
+
+}
