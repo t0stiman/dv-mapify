dddf71938966cef374e41120b06776537559bf6c
t0stiman <18124323+t0stiman@users.noreply.github.com>
2025-12-14 14:02:47 +0100

    fix locomotive spawner CCL compatibility, add B99 new locomotives

--- MapifyEditor/RollingStock/VanillaRollingStockType.cs
+++ MapifyEditor/RollingStock/VanillaRollingStockType.cs
@@ -6,11 +6,15 @@ namespace Mapify.Editor
     {
         DE2 = 0,
         DE6 = 1,
+        // DE6Slug = 2, //Slug spawning sadly does not work
         DH4 = 10,
         DM3 = 20,
+        DM1U = 21,
         S282A = 30,
         S282B = 31,
-        S060 = 40
+        S060 = 40,
+        BE2 = 50,
+        HandCar = 60
     }
 
     public static class VanillaRollingStockTypeExtensions
@@ -20,11 +24,15 @@ namespace Mapify.Editor
             return vanillaRollingStockType switch {
                 VanillaRollingStockType.DE2 => "LocoDE2",
                 VanillaRollingStockType.DE6 => "LocoDE6",
+                // VanillaRollingStockType.DE6Slug => "LocoDE6Slug",
                 VanillaRollingStockType.DH4 => "LocoDH4",
                 VanillaRollingStockType.DM3 => "LocoDM3",
+                VanillaRollingStockType.DM1U => "LocoDM1U",
                 VanillaRollingStockType.S282A => "LocoS282A",
                 VanillaRollingStockType.S282B => "LocoS282B",
                 VanillaRollingStockType.S060 => "LocoS060",
+                VanillaRollingStockType.BE2 => "LocoMicroshunter",
+                VanillaRollingStockType.HandCar => "HandCar",
                 _ => throw new ArgumentOutOfRangeException(nameof(vanillaRollingStockType), vanillaRollingStockType, null)
             };
         }
