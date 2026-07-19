6d252479507f5fba22968c93a3d23eaf4482d6ec
t0stiman <18124323+t0stiman@users.noreply.github.com>
2026-07-19 09:13:50 +0200

    fix derailments closes #14

---
+++ Mapify/Patches/DerailedTrainCarIsKinematicHandlerPatch.cs
@@ -0,0 +1,17 @@
+﻿using DV;
+using HarmonyLib;
+using Mapify.Map;
+
+namespace Mapify.Patches;
+
+/// <summary>
+/// Without this patch, trains instantly come to a stop when derailing
+/// </summary>
+[HarmonyPatch(typeof(DerailedTrainCarIsKinematicHandler), nameof(DerailedTrainCarIsKinematicHandler.UpdateIsKinematicDependingOnLoadedCells ))]
+public static class DerailedTrainCarIsKinematicHandler_UpdateIsKinematicDependingOnLoadedCells_Patch
+{
+    private static bool Postfix()
+    {
+        return false;
+    }
+}
